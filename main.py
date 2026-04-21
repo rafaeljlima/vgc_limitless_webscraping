559f28c7221655b1c10505c11be12d539be52e59
Rafael Jardim <rafaelj13.lima@gmail.com>
2026-04-21 17:04:56 -0300

    Comentando o código

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
+#Importando bibliotecas e dependências do Selenium
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+#Importando funções de outros arquivos
 from database import Database
 from db_functions import *
 
+#Função de scraping
 def scrape_all_tournaments():
+    #Carregando a url desejada
     url = "https://play.limitlesstcg.com/tournaments/completed?game=VGC&format=M-A&platform=all&type=online&time=4weeks"
 
+    #Definindo opções acessadas para o navegador, nesse caso headless-new e com gpu desativada para evitar bugs e diminuir processamento
     options = Options()
     options.add_argument("--headless=new")
     options.add_argument("--disable-gpu")
@@ -21,10 +26,14 @@ def scrape_all_tournaments():
 
     try:
         driver.get(url)
+
+        #Utilizando sempre wait until para cada pagina carregar antes de começar o scraping
         wait.until(EC.presence_of_element_located((By.CLASS_NAME, "completed-tournaments")))
 
+        #Utilizando a conexão do banco
         with db.connect() as cursor:
 
+            #Definindo tabelas e linhas de acordo com o HTML da página
             table = driver.find_element(By.CLASS_NAME, "completed-tournaments")
             rows = table.find_elements(By.TAG_NAME, "tr")[1:]
 
@@ -36,23 +45,29 @@ def scrape_all_tournaments():
 
                 cols = row.find_elements(By.TAG_NAME, "td")
 
+                #Pegando cada elemento do torneio
                 date = cols[1].text
                 name = cols[2].text
                 players = cols[5].text
 
+                #Convertendo a data para formato sql
                 date_mysql = convert_date_to_mysql(date)
 
+                #Inserindo os dados extraídos do torneio no banco
+                tournament_id = insert_tournament(cursor, name, date_mysql, players)
+
+                #Clicando no torneio
                 cols[2].find_element(By.TAG_NAME, "a").click()
 
                 wait.until(EC.presence_of_element_located((By.CLASS_NAME, "striped")))
 
-                tournament_id = insert_tournament(cursor, name, date_mysql, players)
-
+                #Carregamos a tabela dos jogadores do torneio
                 players_table = wait.until(EC.presence_of_element_located((By.CLASS_NAME, "striped")))
                 player_rows = players_table.find_elements(By.TAG_NAME, "tr")[1:]
 
                 for i in range(len(player_rows)):
-
+                    
+                    #Extraimos e inserimos as informações do jogador e resultados do time
                     players_table = wait.until(EC.presence_of_element_located((By.CLASS_NAME, "striped")))
                     player_rows = players_table.find_elements(By.TAG_NAME, "tr")[1:]
                     prow = player_rows[i]
@@ -68,6 +83,7 @@ def scrape_all_tournaments():
 
                     team_id = insert_team(cursor, tournament_id, player_id, wins, losses, draws)
 
+                    #Clicando para seguir com a extração do time
                     cols[-1].find_element(By.TAG_NAME, "a").click()
 
                     wait.until(EC.presence_of_element_located((By.CLASS_NAME, "teamlist-pokemon")))
@@ -75,6 +91,7 @@ def scrape_all_tournaments():
                     team_div = wait.until(EC.presence_of_element_located((By.CLASS_NAME, "teamlist-pokemon")))
                     pokemons = team_div.find_elements(By.CLASS_NAME, "pkmn")
 
+                    #Pegando os detalhes de cada pokémon do time
                     for p in pokemons:
                         pokemon_name = p.find_element(By.CSS_SELECTOR, ".name span").text
                         item = p.find_element(By.CLASS_NAME, "item").text
@@ -86,6 +103,7 @@ def scrape_all_tournaments():
                         pt_id = insert_pokemon_team(cursor, team_id, pokemon_name, item, ability)
                         insert_moves(cursor, pt_id, moves)
 
+                    #Voltando para páginas anteriores, para reiniciar o processo e procurar por mais jogadore/times e depois por mais torneios
                     driver.back()
                     wait.until(EC.presence_of_element_located((By.CLASS_NAME, "striped")))
 
@@ -94,5 +112,6 @@ def scrape_all_tournaments():
     finally:
         driver.quit()
 
+#Evitando execução adicional
 if __name__ == "__main__":
     scrape_all_tournaments()
